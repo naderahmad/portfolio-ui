b45a3c9b6d1e205a09fea54c3d80e0f5d22275fd
Nader Ahmad <naderahmad7x@gmail.com>
2025-01-25 12:57:28 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://samerahmad.com',
+  base: 'portfolio-ui',
   integrations: [mdx(), sitemap(), tailwind()]
 });
